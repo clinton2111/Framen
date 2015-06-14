aa42aaea9538b329d691cb5e2c6fdd89f78db836
Clinton D'souza <clinton_dsouza92@yahoo.com>
2015-06-15 01:05:50 +0530

    Added SendGrid Integration

--- api/source/mailer.php
+++ api/source/mailer.php
@@ -1,26 +1,40 @@
 <?php
 
-require_once '../vendor/phpmailer/phpmailer/PHPMailerAutoload.php';
+require_once '../vendor/autoload.php';
+$mail = new PHPMailer();
+$mail->isSMTP();
+$mail->SMTPAuth = true;
+$mail->CharSet = 'UTF-8';
+
+$mail->Host = 'smtp.sendgrid.net';
+$mail->Username = 'aventador_21';
+$mail->Password = '';
+
+$mail->SMTPSecure = 'ssl';
+$mail->Port = 465;
+
 $json = file_get_contents('php://input');
 $data = json_decode($json);
 
 $response = array();
-$mail = new PHPMailer;
 
-//From email address and name
-$mail->From = $data->address;
-$mail->FromName = $data->name;
+////From email address and name
+$mail->From = 'noreply@framin.in';
+$mail->FromName = $data->name . ' (via. Framin.in - Website)';
 
 //To address and name
-$mail->addAddress("clinton92@gmail.com", "Clinton D'souza");
+$mail->addAddress('clinton92@gmail.com', 'Clinton D\'souza');
 
 //Address to which recipient will reply
-$mail->addReplyTo($data->address, "Reply");
+$mail->addReplyTo(trim($data->address), $data->name);
+
+// indicates ReturnPath header
+$mail->Sender = trim($data->address);
 
 //Send HTML or Plain Text email
 $mail->isHTML(false);
 
-$mail->Subject = "Sent from Framen.in";
+$mail->Subject = $data->subject;
 $mail->Body = htmlentities($data->msg);
 $mail->AltBody = htmlentities($data->msg);
 
@@ -32,3 +46,5 @@ if (!$mail->send()) {
     $response['message'] = 'Message Sent';
 }
 echo json_encode($response);
+
+
